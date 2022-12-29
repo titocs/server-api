2532d4cf22f53170dea74311e435d1b138c838e9
Hfn <55591161+titocs@users.noreply.github.com>
2022-12-29 10:07:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 let routes = require('./routes/routes');
 routes(app);
 
-app.listen(3000, () => {
+app.listen(port, () => {
 	console.log(`Server started on port`);
 });
