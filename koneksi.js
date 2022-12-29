a41e7616a1b92053e1bb874bf61013b2b944b4b8
Hfn <55591161+titocs@users.noreply.github.com>
2022-12-29 10:36:24 +0700

    Update koneksi.js

--- koneksi.js
+++ koneksi.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 // buat koneksi database
 const conn = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'lovej'
+	host: 'sql6.freesqldatabase.com',
+	user: 'sql6587015',
+	password: 'zUHF6ZC8fi',
+	database: 'sql6587015'
 });
 
 conn.connect((err) => {
