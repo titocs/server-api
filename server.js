1cef2659d211ff612832f241d2e228f7afae0c18
Hfn <55591161+titocs@users.noreply.github.com>
2022-12-29 12:31:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const cors = equire('cors');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
